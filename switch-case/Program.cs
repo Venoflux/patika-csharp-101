a9acc2f35a343f7b321a3c3af5be9990865b177c
Erol Sarp Konaklioglu <erolsarpk@gmail.com>
2021-10-13 17:00:44 +0200

    switch-case

---
+++ switch-case/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace switch_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month;
+
+            // Expression
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("Ocak ayındasınız.");
+                    break;
+                case 2:
+                    Console.WriteLine("Şubat ayındasınız.");
+                    break;
+                case 4: // Sıra fark etmez
+                    Console.WriteLine("Nisan ayındasınız.");
+                    break;
+                case 3:
+                    Console.WriteLine("Mart ayındasınız.");
+                    break;
+                default:
+                    Console.WriteLine("Yanlış veri girdiniz!");
+                    break;
+
+            }
+
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Kış ayındasınız");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("İlkbahar ayındasınız");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Yaz ayındasınız");
+                    break;
+                default: // Uyan case direk gerçekleşir default ancak hiçbiri olmadıktan sonra devreye girer yerin bir önemi yok
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Sonbahar ayındasınız");
+                    break;
+                
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
